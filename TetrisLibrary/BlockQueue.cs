688f4d1a2e283c6915faf6c50a2d4a019e13362b
cxfow <cfowler1219@gmail.com>
2024-10-10 16:44:50 -0400

    Add project files.

---
+++ TetrisLibrary/BlockQueue.cs
@@ -0,0 +1,45 @@
+﻿
+using TetrisLibrary.BlockTypes;
+
+namespace TetrisLibrary;
+
+public class BlockQueue
+{
+    private readonly BlockFormat[] blocks =
+    [
+        new IBlock(),
+        new JBlock(),
+        new LBlock(),
+        new OBlock(),
+        new SBlock(),
+        new TBlock(),
+        new ZBlock()
+    ];
+
+    private readonly Random _random = new();
+
+    public BlockFormat NextBlock { get; private set; }
+
+    public BlockQueue()
+    {
+        NextBlock = RandomBlock();
+    }
+
+    private BlockFormat RandomBlock()
+    {
+        return blocks[_random.Next(blocks.Length)];
+    }
+
+    public BlockFormat GetAndUpdate()
+    {
+        BlockFormat block = NextBlock;
+
+        do
+        {
+            NextBlock = RandomBlock();
+        }
+        while (block.BlockID == NextBlock.BlockID);
+
+        return block;
+    }
+}
---
+++ TetrisLibrary/BlockTypes/IBlock.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TetrisLibrary.BlockTypes
+{
+    public class IBlock : BlockFormat
+    {
+        public override int BlockID => 1;
+        protected override Position InitialOffset => new(-1, 3);
+        protected override Position[][] Tiles => tiles;
+
+        private readonly Position[][] tiles =
+        [
+            [new(1,0), new(1,1), new(1,2), new(1,3)],
+            [new(0,2), new(1,2), new(2,2), new(3,2)],
+            [new(2,0), new(2,1), new(2,2), new(2,3)],
+            [new(0,1), new(1,1), new(2,1), new(3,1)]
+        ];
+    }
+}
